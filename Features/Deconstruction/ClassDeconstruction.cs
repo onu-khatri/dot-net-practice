cbc4e79ffa522c25141b6b33248d9bcfbad3dcf5
Anup Singh <anup.singh@careerbuilder.com>
2025-06-23 13:56:45 +0530

    add deconstruction examples

---
+++ Features/Deconstruction/ClassDeconstruction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.Deconstruction
+{
+    internal class PersonalInformation() {
+        public required string FirstName { get; set; }
+        public string? City { get; set; }
+        public required string State { get; set; }
+
+        public void Deconstruct(out string firstName, out string city)
+        {
+            firstName = FirstName;
+            city = City ?? "NaN";
+        }
+
+        public void Deconstruct(out string firstName, out string city, out string state)
+        {
+            firstName = FirstName;
+            city = City ?? "NaN";
+            state = State;
+        }
+    }
+
+
+    internal class ClassDeconstruction
+    {
+        public static void PrintPersonalInfoText(PersonalInformation personalInformation)
+        {
+            var (firstName, city) = personalInformation;
+            Console.WriteLine($"Available Info: {firstName} lives in {city}");
+
+            personalInformation.State = "Delhi";
+
+            (firstName, city, string state) = personalInformation;
+
+            Console.WriteLine($"Updated info: {firstName} lives in {city} of {state}");  
+
+        }
+
+    }
+}
---
+++ Features/Deconstruction/TupleDeconstruction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.Deconstruction
+{
+    record UserLocation(string City, string State, String Country);
+
+    internal class TupleDeconstruction
+    {
+        public static void PrintLocationText(UserLocation location)
+        {
+            string country = "India";
+
+            (string city, var state, country) = location; // When you declare a record type by using two or more positional parameters, the compiler creates a Deconstruct method with an out parameter for each positional parameter in the record declaration.
+
+            var (citySuffixed, stateSuffixed, _) = SuffixLocation(city, state, country);
+
+
+            Console.WriteLine($"User-Location: {citySuffixed}-{stateSuffixed}-{country}");
+        }
+
+        public static void PrintLocationText()
+        {
+            string country = "India";
+
+            var location = ("New-Delhi", state: "Delhi", country); // Tuple assignment
+
+            var (citySuffixed, stateSuffixed, _) = SuffixLocation(location.Item1, location.state, country);
+
+
+            Console.WriteLine($"Default-Location: {citySuffixed}-{stateSuffixed}-{country}");
+        }
+
+        private static (string city, string state, string country) SuffixLocation(string city, string state, string country) { 
+        
+            return (city+"_city", state+"_state", country+"_country");
+        }
+    }
+}
